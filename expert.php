4c314eaf909ef50559c9f24048ac2a25ae3a7356
Manten Devillé <manten.d@hotmail.com>
2021-01-05 12:17:39 +0100

    Finished 3rd exercise, length doesn't make sense yet

--- expert.php
+++ expert.php
@@ -14,9 +14,30 @@ declare(strict_types=1);
 echo "Exercise 1 starts here:";
 
 function new_exercise() {
+    $x = 1;
     $block = "<br/><hr/><br/><br/>Exercise $x starts here:<br/>";
     echo $block;
 }
+
+new_exercise(2);
+// === Exercise 2 ===
+// Below we create a week array with all days of the week.
+// We then try to print the first day which is monday, execute the code and see what happens.
+
+$week = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+$monday = $week[0];
+
+echo $monday;
+
+new_exercise(3);
+// === Exercise 3 ===
+// This should echo ` "Debugged !" `, fix it so that that is the literal text echo'ed
+
+$str ='“Debugged !" Also very fun”';
+echo substr($str, 0, 14);
+
+
+
 
 
 
